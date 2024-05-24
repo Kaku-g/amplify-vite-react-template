3f79966be8fa4cf2ee0f93ba6e0124a0dae4f92b
Jatin <152367689+jatinr-edviron@users.noreply.github.com>
2024-05-25 00:05:51 +0530

    file accessible

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,36 @@ import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
 import { Authenticator } from "@aws-amplify/ui-react";
 import { FileUploader } from "./components/FileUploader";
+import { list } from "aws-amplify/storage";
 
 const client = generateClient<Schema>();
 
 function App() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+  const [data, setData] = useState<any>([]);
 
   useEffect(() => {
     client.models.Todo.observeQuery().subscribe({
       next: (data) => setTodos([...data.items]),
     });
+  }, []);
+
+  useEffect(() => {
+    const getFolder = async () => {
+      try {
+        const result = await list({
+          path: ({ identityId }) => `protected/${identityId}/`,
+        });
+        if (result) {
+          setData(result);
+          console.log(result);
+        }
+      } catch (e) {
+        console.log(e);
+      }
+    };
+
+    getFolder();
   }, []);
 
   function createTodo() {
@@ -39,6 +59,7 @@ function App() {
           </div>
           <FileUploader />
           <h2>{user?.username}</h2>
+          <h3>Files {data ? "yes" : "no"}</h3>
           <button onClick={signOut}>Sign Out</button>
         </main>
       )}
