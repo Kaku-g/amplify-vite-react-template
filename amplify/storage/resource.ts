978a9e5cd47a133dc66a4b762447cc45ca07e84b
Jatin <152367689+jatinr-edviron@users.noreply.github.com>
2024-05-24 18:03:08 +0530

    storage-permissions

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -3,9 +3,10 @@ import { defineStorage } from "@aws-amplify/backend";
 export const storage = defineStorage({
   name: "amplifyTeamDrive",
   access: (allow) => ({
-    "protected/{identityId}/*": [
+    "protected/{entity_id}/*": [
       allow.guest.to(["read"]),
       allow.entity("identity").to(["read", "write", "delete"]),
+      allow.authenticated.to(["read"]),
     ],
   }),
 });
